22251b4ce4ad74d5627fb22514c4c8bc17d95aa2
kimD0ngjun <chickentasty0112@gmail.com>
2025-06-02 13:56:46 +0900

    initial setting

---
+++ src/main/java/org/jun/saemangeum/SaemangeumApplication.java
@@ -0,0 +1,13 @@
+package org.jun.saemangeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaemangeumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaemangeumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jun/saemangeum/SaemangeumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jun.saemangeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaemangeumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
