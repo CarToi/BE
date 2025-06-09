70429a8be33c4d0dc7dd64da00151b2e3f3240ed
kimD0ngjun <chickentasty0112@gmail.com>
2025-06-09 15:11:05 +0900

    chore: JPA, H2 세팅

--- src/main/java/org/jun/saemangeum/global/persistence/domain/Content.java
+++ src/main/java/org/jun/saemangeum/global/persistence/domain/Content.java
@@ -1,10 +1,29 @@
 package org.jun.saemangeum.global.persistence.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 // H2의 엔티티로 쓰이게 될 클래스
+@Entity
+@Table(name = "contents")
+@Getter
+@NoArgsConstructor
 public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Category category;
+
+    @Column
     private String image;
+
+    @Column
     private String introduction;
 }
