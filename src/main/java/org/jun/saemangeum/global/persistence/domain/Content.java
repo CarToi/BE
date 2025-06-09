7b4f1f85264647b1741c1f102f1663b5606d5062
kimD0ngjun <chickentasty0112@gmail.com>
2025-06-09 15:30:06 +0900

    feat: DTO -> 엔티티 전환 로직 추가

--- src/main/java/org/jun/saemangeum/global/persistence/domain/Content.java
+++ src/main/java/org/jun/saemangeum/global/persistence/domain/Content.java
@@ -1,14 +1,19 @@
 package org.jun.saemangeum.global.persistence.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.jun.saemangeum.process.domain.dto.RefinedDataDTO;
 
 // H2의 엔티티로 쓰이게 될 클래스
 @Entity
+@Builder
 @Table(name = "contents")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Content {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +31,13 @@ public class Content {
 
     @Column
     private String introduction;
+
+    public static Content create(RefinedDataDTO dto) {
+        return Content.builder()
+                .title(dto.title())
+                .category(dto.category())
+                .image(dto.image())
+                .introduction(dto.introduction())
+                .build();
+    }
 }
