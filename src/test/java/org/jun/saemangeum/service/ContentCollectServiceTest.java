c5847d109fb7d4fa34532837c3f59732cbc184a8
kimD0ngjun <chickentasty0112@gmail.com>
2025-06-11 11:32:24 +0900

    test: 가상 스레드풀 로직 단위 테스트

--- src/test/java/org/jun/saemangeum/service/ContentCollectServiceTest.java
+++ src/test/java/org/jun/saemangeum/service/ContentCollectServiceTest.java
@@ -1,6 +1,7 @@
 package org.jun.saemangeum.service;
 
 import org.jun.saemangeum.global.domain.Content;
+import org.jun.saemangeum.global.service.ContentService;
 import org.jun.saemangeum.process.application.service.ContentCollectService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,11 +17,25 @@ public class ContentCollectServiceTest {
     @Autowired
     private ContentCollectService contentCollectService;
 
+    @Autowired
+    private ContentService contentService;
+
     @Test
     @DisplayName("서비스 일괄 전처리 및 저장 확인용 단위 테스트")
     void testContentCollectService() {
         contentCollectService.collectAndSave();
-        List<Content> contents = contentCollectService.getContents();
+        List<Content> contents = contentService.getContents();
+
+        // 현재까지 수집한 데이터들이 157개긴한데 얘는 나중에 폐기해야 할듯
+        Assertions.assertEquals(contents.size(), 157);
+        Assertions.assertEquals(contents.getFirst().getClass(), Content.class);
+    }
+
+    @Test
+    @DisplayName("가상 스레드풀 기반 데이터 저장 확인용 단위 테스트")
+    void testContentCollectServiceByVirtualThread() {
+        contentCollectService.collectAndSaveAsync().join(); // 비동기식 저장이므로 저장 끝날때까지 기다리기
+        List<Content> contents = contentService.getContents();
 
         // 현재까지 수집한 데이터들이 157개긴한데 얘는 나중에 폐기해야 할듯
         Assertions.assertEquals(contents.size(), 157);
