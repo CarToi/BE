df285bc1445040d659b36e05feaa9034e6d56414
kimD0ngjun <chickentasty0112@gmail.com>
2025-06-30 19:23:10 +0900

    test: 테이블 -> 뷰 튜플 및 연관관계 임시 이관 확인

--- src/main/java/org/jun/saemangeum/consume/domain/swap/VectorView.java
+++ src/main/java/org/jun/saemangeum/consume/domain/swap/VectorView.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 
 @Getter
 @Entity
-@Table(name = "vectors_view")
+@Table(
+        name = "vectors_view",
+        indexes = @Index(name = "fk_vector_view", columnList = "content_id") // 외래키 제약조건 제거로 직접 인덱스 세팅
+)
 public class VectorView {
     @Id
     private Long id;
@@ -15,6 +18,11 @@ public class VectorView {
     private byte[] vector;
 
     @OneToOne
-    @JoinColumn(name = "content_id")
+    @JoinColumn(
+            name = "content_id",
+            insertable = false,
+            updatable = false,
+            foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT) // 외래키 제약조건 제거
+    )
     private ContentView contentView;
 }
